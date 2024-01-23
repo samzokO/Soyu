dbb85b2f1d47ac27244f54a1cdaa4233fc70a09d
손주현 <juhyeon042@naver.com>
2024-01-23 18:33:19 +0900

    feat: Naver properties 구현

---
+++ back/src/main/java/com/ssafy/soyu/util/naver/NaverProperties.java
@@ -0,0 +1,15 @@
+package com.ssafy.soyu.util.naver;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.naver")
+public class NaverProperties {
+    private String clientId;
+    private String clientSecret;
+    private String authorizationGrantType;
+}
