0c8977c16e476b7685ac6909c1b0e40b06ea8f95
손주현 <juhyeon042@naver.com>
2024-01-24 00:03:21 +0900

    feat: Error 코드 추가

--- back/src/main/java/com/ssafy/soyu/util/response/ErrorCode.java
+++ back/src/main/java/com/ssafy/soyu/util/response/ErrorCode.java
@@ -15,7 +15,9 @@ public enum ErrorCode {
     EXPIRED_AUTH_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     NOT_FOUND_AUTH_TOKEN(HttpStatus.BAD_REQUEST, "토큰 정보가 없습니다."),
     NAVER_ERROR(HttpStatus.BAD_REQUEST, "네이버 오류입니다."),
-    UNAUTHORIZED_FUNCTION_ACCESS(HttpStatus.UNAUTHORIZED, "로그인 후 이용할 수 있습니다.");
+    UNAUTHORIZED_FUNCTION_ACCESS(HttpStatus.UNAUTHORIZED, "로그인 후 이용할 수 있습니다."),
+    INVALID_AUTH_CODE(HttpStatus.NOT_FOUND, "인증코드가 일치하지 않습니다."),
+    INVALID_AUTH_TOKEN(HttpStatus.UNAUTHORIZED, "권한 정보가 없는 토큰입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
