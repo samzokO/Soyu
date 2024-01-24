5571859ea29c12983ffb6e5bd8b568183d18f7e3
이재신 <jaesin463@gmail.com>
2024-01-24 15:21:40 +0900

    build: 알림 전송 관련 FCM dependencies 추가

--- back/src/main/java/com/ssafy/soyu/notice/Notice.java
+++ back/src/main/java/com/ssafy/soyu/notice/Notice.java
@@ -8,20 +8,20 @@ import lombok.Getter;
 @Table(name = "notice")
 @Getter
 public class Notice {
-    @Id
-    @GeneratedValue
-    @Column(name = "notice_id")
-    private Long id;
+  @Id
+  @GeneratedValue
+  @Column(name = "notice_id")
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "receiver_id")
-    private Member member;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "receiver_id")
+  private Member member;
 
-    private String content;
+  private String content;
 
-    private NoticeStatus status;
+  private NoticeStatus status;
 
-    private Boolean isCheck;
-    private Boolean isDelete;
+  private Boolean isCheck;
+  private Boolean isDelete;
 
 }
