58871e6ec5060b519aa3ac61570dfcc885a6a03a
최준영 <wjdghkswnsdu@naver.com>
2024-01-26 21:11:11 +0900

    refactor: log 를 위한 어노테이션 추가

--- back/src/main/java/com/ssafy/soyu/item/domain/request/ItemStatusRequest.java
+++ back/src/main/java/com/ssafy/soyu/item/domain/request/ItemStatusRequest.java
@@ -2,8 +2,10 @@ package com.ssafy.soyu.item.domain.request;
 
 import com.ssafy.soyu.item.domain.ItemStatus;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class ItemStatusRequest {
   Long itemId;
   ItemStatus itemStatus;
