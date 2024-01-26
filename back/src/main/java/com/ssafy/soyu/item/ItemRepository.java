f4e9f11ab6747a7feb9751a7da18a7c79d824595
손주현 <juhyeon042@naver.com>
2024-01-26 17:24:45 +0900

    feat: Item status 업데이트 구현

--- back/src/main/java/com/ssafy/soyu/item/ItemRepository.java
+++ back/src/main/java/com/ssafy/soyu/item/ItemRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.ssafy.soyu.member.domain.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,6 +18,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
   List<Item> findItemByMember(Member member);
 
   List<Item> findItemByItemCategories(ItemCategories itemCategories);
+
   @Query("select i from Item i join fetch i.member m where i.member.id = :memberId")
   List<Item> findByMemberId(Long memberId);
+
+  @Modifying
+  @Query("UPDATE Item SET itemStatus= :status where id=:itemId")
+  int updateStatus(Long itemId, ItemStatus status);
 }
