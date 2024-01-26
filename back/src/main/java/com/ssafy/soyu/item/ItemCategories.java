0fb6236bf09e5128b866ae80264c1e18ea1cf469
최준영 <wjdghkswnsdu@naver.com>
2024-01-26 12:46:54 +0900

    feat: Item 카테고리 데이터 추가

--- back/src/main/java/com/ssafy/soyu/item/ItemCategories.java
+++ back/src/main/java/com/ssafy/soyu/item/ItemCategories.java
@@ -1,5 +1,13 @@
 package com.ssafy.soyu.item;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum ItemCategories {
+  // 차례로 가전 , 옷, 가구, 책, 스포츠
+  Electronics, Clothing, Furniture, Books, Sports;
+  @JsonCreator
+  public static ItemCategories from(String s) {
+    return ItemCategories.valueOf(s.toUpperCase());
+  }
 
 }
