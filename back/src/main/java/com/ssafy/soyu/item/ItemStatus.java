9715bb4e70af8a4429461efec50bd34487a703b3
손주현 <juhyeon042@naver.com>
2024-01-26 17:23:11 +0900

    refactor: enum 이름 변경

--- back/src/main/java/com/ssafy/soyu/item/ItemStatus.java
+++ back/src/main/java/com/ssafy/soyu/item/ItemStatus.java
@@ -3,8 +3,8 @@ package com.ssafy.soyu.item;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum ItemStatus {
-  // 차례로 온라인, DP, 판매완료, 삭제됨;
-  online, dp, sold, deleted;
+  // 차례로 온라인, DP, 판매완료, 삭제됨, 예약중;
+  ONLINE, DP, SOLD, DELETED, RESERVE;
   @JsonCreator
   public static ItemStatus from(String s) {
     return ItemStatus.valueOf(s.toUpperCase());
