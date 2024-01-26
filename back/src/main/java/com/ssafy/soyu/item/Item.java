21a77927c4db6de1475f6c1d232e1ebdbe535c03
최준영 <wjdghkswnsdu@naver.com>
2024-01-26 12:44:39 +0900

    feat: Item 엔티티에 생성자와 팩토리 메서드 추가

--- back/src/main/java/com/ssafy/soyu/item/Item.java
+++ back/src/main/java/com/ssafy/soyu/item/Item.java
@@ -3,6 +3,8 @@ package com.ssafy.soyu.item;
 import com.ssafy.soyu.member.domain.Member;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -10,11 +12,14 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "item")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
   @Id
   @GeneratedValue
@@ -33,8 +38,29 @@ public class Item {
 
   private Integer price;
 
+  @Enumerated(EnumType.STRING)
   private ItemStatus itemStatus;
 
+  @Enumerated(EnumType.STRING)
   private ItemCategories itemCategories;
+
+  public Item(Member member, String title, String content, Integer price, ItemCategories itemCategories ) {
+    this.member = member;
+    this.title = title;
+    this.content = content;
+    this.price = price;
+    this.itemCategories = itemCategories;
+  }
+
+  public void updateItem(String title, String content, Integer price, ItemCategories itemCategories ) {
+    this.title = title;
+    this.content = content;
+    this.price = price;
+    this.itemCategories = itemCategories;
+  }
+
+  public void updateItemStatus(ItemStatus itemStatus) {
+    this.itemStatus = itemStatus;
+  }
 
 }
