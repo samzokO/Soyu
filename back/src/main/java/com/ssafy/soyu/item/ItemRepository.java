eec67f7f59572d23811dbe4e044e03227b10fcaa
최준영 <wjdghkswnsdu@naver.com>
2024-01-26 12:45:26 +0900

    feat: ItemRepository Repository 설계

--- back/src/main/java/com/ssafy/soyu/item/ItemRepository.java
+++ back/src/main/java/com/ssafy/soyu/item/ItemRepository.java
@@ -2,11 +2,21 @@ package com.ssafy.soyu.item;
 
 import com.ssafy.soyu.history.dto.response.SaleResponseDto;
 import java.util.List;
+import com.ssafy.soyu.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+  @Query("select i from Item i where i.title like %:keyword%")
+  List<Item> findItemByKeyWord(@Param("keyword") String keyword);
+
+  List<Item> findItemByMember(Member member);
+
+  List<Item> findItemByItemCategories(ItemCategories itemCategories);
   @Query("select i from Item i join fetch i.member m where i.member.id = :memberId")
   List<Item> findByMemberId(Long memberId);
 }
