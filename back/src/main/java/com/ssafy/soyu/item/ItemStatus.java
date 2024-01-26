9ddb9bbc3f076a7bb92747cbb843dcdba7aba6cf
최준영 <wjdghkswnsdu@naver.com>
2024-01-26 12:47:09 +0900

    feat: Item 상태 데이터 추가

--- back/src/main/java/com/ssafy/soyu/item/ItemStatus.java
+++ back/src/main/java/com/ssafy/soyu/item/ItemStatus.java
@@ -1,4 +1,12 @@
 package com.ssafy.soyu.item;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum ItemStatus {
+  // 차례로 온라인, DP, 판매완료, 삭제됨;
+  online, dp, sold, deleted;
+  @JsonCreator
+  public static ItemStatus from(String s) {
+    return ItemStatus.valueOf(s.toUpperCase());
+  }
 }
