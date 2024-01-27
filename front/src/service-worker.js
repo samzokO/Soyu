aecb78409abd711cbbc6a9d03ebcea0da432fc1e
이호진 <skwo777@naver.com>
2024-01-28 04:32:06 +0900

    feat: Service Worker 작성

--- front/src/service-worker.js
+++ front/src/service-worker.js
@@ -71,3 +71,13 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
