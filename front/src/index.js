6deca297bbaf7054c8cfb6a96f4dff8427c95207
이호진 <skwo777@naver.com>
2024-01-28 16:58:43 +0900

    style: CSS reset, font 추가

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import GlobalStyles from './styles/GlobalStyles';
+import './utils/firebase-messaging-sw';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <RouterProvider router={Router} />
+  </ThemeProvider>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
