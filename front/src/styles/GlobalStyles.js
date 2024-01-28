9079255db9e3140e0fbe78ea5f5d635f295fc9cc
이호진 <skwo777@naver.com>
2024-01-29 02:53:14 +0900

    style: 기본 색상 작성

--- front/src/styles/GlobalStyles.js
+++ front/src/styles/GlobalStyles.js
@@ -7,7 +7,7 @@ const GlobalStyles = createGlobalStyle`
     *{margin:0;padding:0;font:inherit;color:inherit;}
     *, :after, :before {box-sizing:border-box;}
     :root {-webkit-tap-highlight-color:transparent;-webkit-text-size-adjust:100%;text-size-adjust:100%;cursor:default;line-height:1.5;overflow-wrap:break-word;word-break:break-word;tab-size:4}
-    html, body {height:100; font-family: 'Pretendard'; font-weight: 600;}
+    html, body {background-color: ${(props) => props.theme.bgColor}; font-family: 'Pretendard'; font-weight: 600;}
     img, picture, video, canvas, svg {display: block;max-width:100%;}
     button {background:none;border:0;cursor:pointer;}
     a {text-decoration:none}
--- front/src/styles/theme.js
+++ front/src/styles/theme.js
@@ -1,5 +1,30 @@
 export const theme = {
-  bgColor: '#2c2c2e',
+  // 백그라운드 컬러
+  bgColor: '#F8F8F8',
+
+  // 메인, 서브 컬러
+  primaryColor: '#4827E9',
+  secondaryColor: '#FFC90B',
+
+  // 무채색
+  white: '#ffffff',
+  grayScale100: '#F8F8F8',
+  grayScale200: '#D8D8D8',
+  grayScale300: '#A2A2A2',
+  grayScale400: '#6F6F6F',
+  grayScale500: '#191919',
+
+  // 인터랙션
+  error: '#D32F2F',
+  success: '#4CAF50',
+  action: '#0091EA',
+  warning: '#F9A825',
+  cancel: '#808080',
+  delete: '#FF4B47',
+
+  // 폰트
+  fontTitle: '#191919',
+  fontBody: '#6F6F6F',
 };
 
 export default theme;
