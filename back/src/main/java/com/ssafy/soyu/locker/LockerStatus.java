4de7181bfb678880987dcfa25bff2b4bb6e18ae1
손주현 <juhyeon042@naver.com>
2024-01-29 23:18:23 +0900

    feat: Locker 상태 추가

--- back/src/main/java/com/ssafy/soyu/locker/LockerStatus.java
+++ back/src/main/java/com/ssafy/soyu/locker/LockerStatus.java
@@ -1,5 +1,9 @@
 package com.ssafy.soyu.locker;
 
 public enum LockerStatus {
-  EMPTY;
+  AVAILABLE , //사용 가능
+  DP,         //DP 중
+  RESERVED, //예약됨 == 판매자가 안넣음
+  WITHDRAW, //회수 대기 중
+  READY; // 판매자가 넣음 == 구매자 픽업 대기
 }
