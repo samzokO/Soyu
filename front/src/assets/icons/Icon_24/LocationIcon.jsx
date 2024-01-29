b542d959ea2f94ffd625c959d584473b8cffe88c
이호진 <skwo777@naver.com>
2024-01-30 02:20:51 +0900

    fix: 아이콘 색상 미적용 수정

--- front/src/assets/icons/Icon_24/LocationIcon.jsx
+++ front/src/assets/icons/Icon_24/LocationIcon.jsx
@@ -9,13 +9,18 @@ const Location = styled.svg`
     xmlns: http://www.w3.org/2000/svg;
   `;
 
+const Path = styled.path`
+  fill: ${(props) =>
+    props.active ? theme.color.grayScale500 : theme.color.grayScale300};
+`;
+
 function LocationIcon({ active }) {
   return (
-    <Location fill="none">
+    <Location active={active}>
       <g clipPath="url(#clip0_539_145)">
-        <path
+        <Path
           d="M12.5 2C8.63 2 5.5 5.13 5.5 9C5.5 12.7317 9.03669 17.7265 11.0823 20.3034C11.8189 21.2314 13.1811 21.2314 13.9177 20.3034C15.9633 17.7265 19.5 12.7317 19.5 9C19.5 5.13 16.37 2 12.5 2ZM12.5 11.5C11.12 11.5 10 10.38 10 9C10 7.62 11.12 6.5 12.5 6.5C13.88 6.5 15 7.62 15 9C15 10.38 13.88 11.5 12.5 11.5Z"
-          fill={active ? 'grayScale500' : 'grayScale300'}
+          active={active}
         />
       </g>
       <defs>
--- front/src/assets/icons/Icon_24/SendIcon.jsx
+++ front/src/assets/icons/Icon_24/SendIcon.jsx
@@ -10,7 +10,7 @@ const IconFill = styled.path`
     props.active ? theme.color.grayScale500 : theme.color.grayScale300};
 `;
 
-function SendIcon() {
+function SendIcon({ active }) {
   return (
     <Send
       xmlns="http://www.w3.org/2000/svg"
@@ -18,13 +18,17 @@ function SendIcon() {
       height="24"
       viewBox="0 0 25 24"
       fill="none"
+      active={active}
     >
       <path
         d="M20.7111 11.1056L4.65934 3.07967C3.86249 2.68124 2.98171 3.44514 3.26344 4.29032L5.72792 11.6838C5.79635 11.889 5.79635 12.111 5.72792 12.3162L3.26344 19.7097C2.98171 20.5549 3.86249 21.3188 4.65934 20.9203L20.7111 12.8944C21.4482 12.5259 21.4482 11.4741 20.7111 11.1056Z"
         strokeWidth="1.5"
         strokeLinejoin="round"
       />
-      <IconFill d="M9.5 12.75C9.91421 12.75 10.25 12.4142 10.25 12C10.25 11.5858 9.91421 11.25 9.5 11.25V12.75ZM5.5 12.75H9.5V11.25H5.5V12.75Z" />
+      <IconFill
+        d="M9.5 12.75C9.91421 12.75 10.25 12.4142 10.25 12C10.25 11.5858 9.91421 11.25 9.5 11.25V12.75ZM5.5 12.75H9.5V11.25H5.5V12.75Z"
+        active={active}
+      />
     </Send>
   );
 }
