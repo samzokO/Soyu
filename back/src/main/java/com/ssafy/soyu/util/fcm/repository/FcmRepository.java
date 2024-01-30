b198f46031be802266b9e604a41e8fa58aa421e5
이재신 <jaesin463@gmail.com>
2024-01-30 10:52:16 +0900

    refactor: fcm repository Param 어노테이션 추가

--- back/src/main/java/com/ssafy/soyu/util/fcm/repository/FcmRepository.java
+++ back/src/main/java/com/ssafy/soyu/util/fcm/repository/FcmRepository.java
@@ -4,9 +4,10 @@ import com.ssafy.soyu.util.fcm.domain.Fcm;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FcmRepository extends JpaRepository<Fcm, Long> {
 
   @Query(value = "select f from Fcm f where f.member.id = :memberId")
-  Optional<Fcm> findByMemberId(Long memberId);
+  Optional<Fcm> findByMemberId(@Param("memberId") Long memberId);
 }
