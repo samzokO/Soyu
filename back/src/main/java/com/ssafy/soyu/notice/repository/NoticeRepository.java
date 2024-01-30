98bcba2333ce924ec68b89050f77a20692ac6a74
이재신 <jaesin463@gmail.com>
2024-01-30 10:51:18 +0900

    refactor: notice repository Param 어노테이션 추가

--- back/src/main/java/com/ssafy/soyu/notice/repository/NoticeRepository.java
+++ back/src/main/java/com/ssafy/soyu/notice/repository/NoticeRepository.java
@@ -5,15 +5,16 @@ import com.ssafy.soyu.notice.dto.response.NoticeResponseDto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
   @Query("select n from Notice n where n.member.id = : memberId")
-  List<Notice> findByMemberId(Long memberId);
+  List<Notice> findByMemberId(@Param("memberId") Long memberId);
 
   @Query("update Notice n set n.status = 'DELETE' where n.id = :noticeId")
-  void deleteNoticeByNoticeId(Long noticeId);
+  void deleteNoticeByNoticeId(@Param("noticeId") Long noticeId);
 
   @Query("update Notice n set n.status = 'READ' where n.id = :noticeId")
-  void readNoticeByNoticeId(Long noticeId);
+  void readNoticeByNoticeId(@Param("noticeId") Long noticeId);
 }
