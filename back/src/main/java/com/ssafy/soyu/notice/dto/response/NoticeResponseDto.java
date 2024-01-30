4ed0a5a0c046fe3d911cef3c647b07ad17d18025
이재신 <jaesin463@gmail.com>
2024-01-30 14:14:20 +0900

    feat: noticeResponseDto 수정

--- back/src/main/java/com/ssafy/soyu/notice/dto/response/NoticeResponseDto.java
+++ back/src/main/java/com/ssafy/soyu/notice/dto/response/NoticeResponseDto.java
@@ -1,8 +1,8 @@
 package com.ssafy.soyu.notice.dto.response;
 
+import com.ssafy.soyu.item.entity.Item;
 import com.ssafy.soyu.notice.domain.Notice;
 import com.ssafy.soyu.notice.domain.NoticeStatus;
-import com.ssafy.soyu.notice.domain.NoticeType;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +13,15 @@ public class NoticeResponseDto {
   private Long noticeId;
   private Long memberId;
   private NoticeStatus status;
-  private NoticeType type;
+  private String type;
+  private Item item;
 
   public NoticeResponseDto(Notice n){
     this.noticeId = n.getId();
     this.memberId = n.getMember().getId();
     this.status = n.getStatus();
     this.type = n.getType();
+    this.item = n.getItem();
   }
 
 }
