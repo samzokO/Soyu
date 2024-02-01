480df1583249916d1972fcef150546fa495054a9
최준영 <wjdghkswnsdu@naver.com>
2024-02-01 15:38:02 +0900

    feat: Image 엔티티 추가

---
+++ back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
@@ -0,0 +1,5 @@
+package com.ssafy.soyu.image.controller;
+
+public class ImageController {
+
+}
---
+++ back/src/main/java/com/ssafy/soyu/image/entity/Image.java
@@ -0,0 +1,47 @@
+package com.ssafy.soyu.image.entity;
+
+import com.ssafy.soyu.item.entity.Item;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@ToString(exclude = {"item"})
+public class Image {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "image_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id")
+  Item item;
+
+  private String savePath;
+
+  private String originalName;
+
+  private String saveName;
+
+  public void makeImage(String today, String originalName, String saveName) {
+    this.savePath = today;
+    this.originalName = originalName;
+    this.saveName = saveName;
+  }
+
+  public void addItem(Item item) {
+    this.item = item;
+    item.getImage().add(this);
+  }
+}
