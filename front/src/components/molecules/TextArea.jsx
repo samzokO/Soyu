67c8e134dff2ccef974b25d4a0b69a59a01c706d
이호진 <skwo777@naver.com>
2024-02-01 09:27:53 +0900

    design: Text Area 라벨링 추가

---
+++ front/src/components/molecules/TextArea.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+function TextArea({ title, id, placeholder }) {
+  return (
+    <SWrap>
+      <SLabel>{title}</SLabel>
+      <SArea id={id} placeholder={placeholder} />
+    </SWrap>
+  );
+}
+
+const SWrap = styled.div`
+  ${({ theme }) => theme.font.Body2};
+`;
+
+const SArea = styled.textarea`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 150px;
+  max-width: 768px;
+  padding: 10px 15px;
+  border-radius: 7px;
+  border: 1px solid ${({ theme }) => theme.color.grayScale400};
+  placeholder: ${(props) => props.placeholder};
+
+  &::placeholder {
+    ${({ theme }) => theme.font.Body2};
+  }
+`;
+
+const SLabel = styled.div`
+  margin: 0px 0px 0px 3px;
+`;
+
+export default TextArea;
