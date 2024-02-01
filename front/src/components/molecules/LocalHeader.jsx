b6fcbb6c57cde686cf055ecf721efef34945dd1b
이호진 <skwo777@naver.com>
2024-02-01 09:28:17 +0900

    refactor: Header 변경

--- front/src/components/molecules/LocalHeader.jsx
+++ front/src/components/molecules/LocalHeader.jsx
@@ -1,13 +1,21 @@
 import styled from 'styled-components';
-import LeftIcon from '../../assets/icons/Icon_24/LeftIcon';
-import TextBtn from '../atoms/TextBtn';
-import BackBtn from '../atoms/BackBtn';
+
+/**  헤더  */
+function LocalHeader({ children }) {
+  return <Header>{children}</Header>;
+}
 
 const Header = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: ${({ theme }) => theme.color.bgColor};
   ${({ theme }) => theme.font.Title};
   padding: 0px 10px;
   height: 44px;
   display: flex;
+  gap: 10px;
   justify-content: space-between;
   align-items: center;
   white-space: nowrap;
@@ -15,19 +23,5 @@ const Header = styled.nav`
   @media screen and (min-width: 769px) {
   }
 `;
-/** 뒤로가기를 포함한 로컬 헤더
- * @params (string) TitleText - 제목,
- * @params (string) BtnText : 버튼내용 */
-function LocalHeader({ TitleText, BtnText }) {
-  return (
-    <Header>
-      <BackBtn>
-        <LeftIcon />
-      </BackBtn>
-      {TitleText}
-      <TextBtn content={BtnText} />
-    </Header>
-  );
-}
 
 export default LocalHeader;
