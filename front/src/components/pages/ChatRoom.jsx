e6b3a349e422a81d802d91abaa12a69151c02b1c
엄진식 <ladiescheong@naver.com>
2024-02-01 17:28:26 +0900

    design: 채팅방 페이지 구현

--- front/src/components/pages/ChatRoom.jsx
+++ front/src/components/pages/ChatRoom.jsx
@@ -1,3 +1,15 @@
-function ChatRoom() {}
+import ChatBottomNav from '../molecules/ChatBottomNav';
+import ChatHeader from '../molecules/ChatHeader';
+import ChatMessageList from '../molecules/ChatMessageList';
+
+function ChatRoom() {
+  return (
+    <>
+      <ChatHeader />
+      <ChatMessageList />
+      <ChatBottomNav />
+    </>
+  );
+}
 
 export default ChatRoom;
