5ef797d6e7d62d2d5f8b263981b390e632090d08
이호진 <skwo777@naver.com>
2024-02-01 09:25:34 +0900

    design: 레이아웃 컨테이너 추가

---
+++ front/src/styles/Maincontainer.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MainContainerWithoutNav = styled.main`
+  margin-top: 44px;
+  padding: 0px 15px;
+`;
+
+export const MainContainerWithNav = styled(MainContainerWithoutNav)`
+  margin-bottom: 53px;
+`;
