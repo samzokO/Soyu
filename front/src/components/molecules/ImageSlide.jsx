ed5be62ea69887c85d0e86ca29240e4d9d562639
이호진 <skwo777@naver.com>
2024-02-01 09:29:22 +0900

    design: 상품 상세 페이지 추가, 기능 제외

---
+++ front/src/components/molecules/ImageSlide.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function ImageSlide() {
+  return <Container />;
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: red;
+`;
+
+export default ImageSlide;
