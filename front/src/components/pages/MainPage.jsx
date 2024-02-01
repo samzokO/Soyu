d3986f2056cf5ccbf302e15760bcccbeb427bad5
이호진 <skwo777@naver.com>
2024-02-01 22:41:46 +0900

    design: Header 아이콘 변경

--- front/src/components/pages/MainPage.jsx
+++ front/src/components/pages/MainPage.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/icons/Logo.svg';
+import Search from '../../assets/icons/material_16/search.svg';
+import Category from '../../assets/icons/material_24/category.svg';
+import Notification from '../../assets/icons/material_24/notification.svg';
 import LocalHeader from '../molecules/LocalHeader';
 import { MainContainerWithNav } from '../../styles/Maincontainer';
 import BottomNav from '../molecules/BottomNav';
@@ -13,9 +16,15 @@ function MainPage() {
           <SLogo src={Logo} alt="로고" />
         </Link>
         <SMenuBox>
-          <Link to="/">메뉴</Link>
-          <Link to="/">메뉴</Link>
-          <Link to="/">메뉴</Link>
+          <Link to="/">
+            <SMenu src={Search} alt="로고" />
+          </Link>
+          <Link to="/">
+            <SMenu src={Category} alt="로고" />
+          </Link>
+          <Link to="/">
+            <SMenu src={Notification} alt="로고" />
+          </Link>
         </SMenuBox>
       </LocalHeader>
       <MainContainerWithNav>dsd</MainContainerWithNav>
@@ -27,6 +36,11 @@ function MainPage() {
 const SLogo = styled.img`
   width: 32px;
   height: 32px;
+`;
+
+const SMenu = styled.img`
+  width: 24px;
+  height: 24px;
 `;
 
 const SMenuBox = styled.div`
