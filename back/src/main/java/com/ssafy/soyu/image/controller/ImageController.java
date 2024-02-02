3b270b04e3c29a1de4e1ed773a3089abc96935ba
최준영 <wjdghkswnsdu@naver.com>
2024-02-02 13:22:14 +0900

    feat: 파일 요청에 데이터 응답 구현

--- back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
+++ back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
@@ -1,5 +1,28 @@
 package com.ssafy.soyu.image.controller;
 
-public class ImageController {
+import static com.ssafy.soyu.util.response.CommonResponseEntity.getResponseEntity;
 
+import com.ssafy.soyu.util.response.SuccessCode;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("image")
+public class ImageController {
+  String uploadImagePath = "C:/board/upload/imageUpload";
+
+  @GetMapping("/{sfolder}/{sfile}")
+  public ResponseEntity<?> getImageFile(@PathVariable("sfolder") String sfolder, @PathVariable("sfile") String sfile) throws IOException {
+    String filePath = uploadImagePath + "/" + sfolder +  "/" + sfile;
+    InputStream imageStream = new FileInputStream(filePath);
+    byte[] imageByteArray = IOUtils.toByteArray(imageStream);
+    imageStream.close();
+
+    return getResponseEntity(SuccessCode.OK, imageByteArray);
+  }
 }
