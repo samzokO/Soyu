f9533248b8f02fcf0def7d5553b79d312ec6ad31
이호진 <skwo777@naver.com>
2024-02-04 17:11:04 +0900

    refactor: 메인 컨테이너 수정 #2

--- front/src/styles/Maincontainer.jsx
+++ front/src/styles/Maincontainer.jsx
@@ -2,13 +2,24 @@ import styled from 'styled-components';
 import { Header } from '../components/molecules/LocalHeader';
 import { SNav } from '../components/molecules/BottomNav';
 
+// 함수를 사용하여 height 값을 추출하는 코드
+const getHeightValue = (component, index) => {
+  const heightStyle =
+    component.componentStyle.rules[index].match(/height:\s*(\d+px);/);
+  return heightStyle ? heightStyle[1] : null;
+};
+
+// Header와 SNav에서 각각 height 값을 추출
+const headerHeight = getHeightValue(Header, 4);
+const sNavHeight = getHeightValue(SNav, 0);
+
 export const MainContainerWithoutNav = styled.main`
   margin-top: 44px;
   padding: 0px 15px;
   position: relative;
-  height: calc(100vh - ${Header.height});
+  height: calc(100vh - ${sNavHeight});
 `;
 
 export const MainContainerWithNav = styled(MainContainerWithoutNav)`
-  height: calc(100vh - ${Header.height} - ${SNav.height});
+  height: calc(100vh - (${headerHeight} + ${sNavHeight}));
 `;
