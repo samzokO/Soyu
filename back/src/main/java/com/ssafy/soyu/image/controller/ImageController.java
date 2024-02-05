089ad9ec3a4e0ad3dfbc6428ba1c20babf71c516
최준영 <wjdghkswnsdu@naver.com>
2024-02-05 16:03:21 +0900

    fix: image data 전달 fix

--- back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
+++ back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
@@ -19,8 +19,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("image")
+@RestController
 public class ImageController {
   @Value("${file.path.upload-images}")
   String uploadImagePath;
