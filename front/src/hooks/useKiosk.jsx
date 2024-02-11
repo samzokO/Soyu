02bc70efae2bf0c9dd126e00e80685231508be8d
이호진 <skwo777@naver.com>
2024-02-12 06:21:36 +0900

    refactor: 키오스크 훅 수정

--- front/src/hooks/useKiosk.jsx
+++ front/src/hooks/useKiosk.jsx
@@ -1,23 +1,44 @@
-import { useState, useEffect } from 'react';
-import { kioskSell, kioskWithdraw } from '../api/apis';
+import { useState } from 'react';
+import { kioskSell, kioskWithdraw, kioskBuy } from '../api/apis';
 
-function useKiosk(type, code) {
-  const [data, setData] = useState('');
-  useEffect(() => {
-    if (type === 'sell') {
-      console.log('판매다');
-      kioskSell(code).then((response) => {
-        const result = response.data.data;
-        setData(result);
-      });
-    } else if (type === 'withdraw') {
-      console.log('회수다');
-      kioskWithdraw(code).then((response) => {
-        const result = response.data.data;
-        setData(result);
-      });
+function useKiosk() {
+  const [data, setData] = useState();
+
+  const selling = (code) => {
+    try {
+      const response = kioskSell(code);
+      setData(response);
+      console.log(response);
+    } catch (error) {
+      console.error('Error while selling:', error);
     }
-  }, [type, code]);
-  return data;
+  };
+
+  const withdrawal = (code) => {
+    try {
+      const response = kioskWithdraw(code);
+      const result = response;
+      setData(result);
+      console.log(result);
+      kioskWithdraw(code).then((res) => {
+        console.log(res);
+      });
+    } catch (error) {
+      console.error('Error while withdrawing:', error);
+    }
+  };
+
+  const purchase = (code) => {
+    try {
+      const response = kioskBuy(1, code);
+      const result = response;
+      setData(result);
+    } catch (error) {
+      console.error('Error while purchasing:', error);
+    }
+  };
+
+  return [data, selling, withdrawal, purchase];
 }
+
 export default useKiosk;
