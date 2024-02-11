7b375ea4b08dd9eb5c61c261263aa21f3874bcd3
이호진 <skwo777@naver.com>
2024-02-12 07:00:13 +0900

    design: framer-motion 적용

--- front/src/components/pages/Login.jsx
+++ front/src/components/pages/Login.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Logo from '../../assets/icons/Logo.svg';
 import KakaoLogin from '../atoms/KakaoLogin';
 import NaverLogin from '../atoms/NaverLogin';
@@ -6,27 +7,72 @@ import GoogleLogin from '../atoms/GoogleLogin';
 import theme from '../../styles/theme';
 
 function Login() {
+  const list = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <SWrap>
-      <img src={Logo} alt="#" />
-      <STitle>소유</STitle>
-      <SNav>
-        <NaverLogin />
-        <KakaoLogin />
-        <GoogleLogin />
+      <motion.img
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 1,
+          delay: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        src={Logo}
+        alt="#"
+      />
+      <STitle
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 1,
+          delay: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        소유
+      </STitle>
+      <SNav variants={list} initial="hidden" animate="visible">
+        <SMdiv variants={item}>
+          <NaverLogin />
+        </SMdiv>
+        <SMdiv variants={item}>
+          <KakaoLogin />
+        </SMdiv>
+        <SMdiv variants={item}>
+          <GoogleLogin />
+        </SMdiv>
       </SNav>
     </SWrap>
   );
 }
 
 export default Login;
+const SMdiv = styled(motion.div)``;
 
-const STitle = styled.h1`
+const STitle = styled(motion.h1)`
   margin: 20px 0;
   ${theme.font.Splash}
 `;
 
-const SWrap = styled.div`
+const SWrap = styled(motion.div)`
   width: 100vw;
   height: 100vh;
 
@@ -36,9 +82,9 @@ const SWrap = styled.div`
   align-items: center;
 `;
 
-const SNav = styled.nav`
+const SNav = styled(motion.nav)`
   width: 200px;
-  & * {
-    margin-bottom: 10px;
-  }
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
