d2654c4d4817d7a7c398a3c009c4da0af093f11a
최준영 <wjdghkswnsdu@naver.com>
2024-02-12 17:14:44 +0900

    refactor: 데이터 랜더링 형식 변경

--- back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
+++ back/src/main/java/com/ssafy/soyu/image/controller/ImageController.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,13 +27,13 @@ public class ImageController {
   String uploadImagePath;
 
   @GetMapping("/{sfolder}/{sfile}")
-  public ResponseEntity<?> getImageFile(@PathVariable("sfolder") String sfolder, @PathVariable("sfile") String sfile) throws IOException {
+  public ResponseEntity<byte[]> getImageFile(@PathVariable("sfolder") String sfolder, @PathVariable("sfile") String sfile) throws IOException {
     String filePath = uploadImagePath + "/" + sfolder +  "/" + sfile;
     InputStream imageStream = new FileInputStream(filePath);
     byte[] imageByteArray = IOUtils.toByteArray(imageStream);
     imageStream.close();
 
-    return getResponseEntity(SuccessCode.OK, imageByteArray);
+    return new ResponseEntity<>(imageByteArray, new HttpHeaders(), HttpStatus.OK);
   }
 
   public static List<ImageResponse> getImageResponse(List<Image> image) {
