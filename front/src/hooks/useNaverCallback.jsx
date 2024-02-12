b9a8795f974c2da37b05c9caacf6fb35594e9d69
엄진식 <ladiescheong@naver.com>
2024-02-12 17:00:38 +0900

    fix: conflit 해결 반영 안된 부분 수정

--- front/src/hooks/useNaverCallback.jsx
+++ front/src/hooks/useNaverCallback.jsx
@@ -11,19 +11,6 @@ function useNaverCallback() {
     const code = searchParams.get('code');
     const state = searchParams.get('state');
     getNaverCode(code, state)
-<<<<<<< HEAD
-      .then((response) => {
-        // spring에서 발급된 jwt 반환 localStorage 저장
-        localStorage.setItem('accessToken', response.data.data.accessToken);
-        localStorage.setItem('refreshToken', response.data.data.refreshToken);
-
-        // 메인 페이지로 이동
-        window.location.href = '/';
-      })
-      .catch(() => {
-        // 에러발생 시 login 페이지로 전환
-        window.location.href = '/login';
-=======
       .then(({ data }) => {
         const token = data.data.accessToken;
         localStorage.setItem('accessToken', token);
@@ -32,7 +19,6 @@ function useNaverCallback() {
       })
       .catch(() => {
         navigate('/login');
->>>>>>> b7e70a5 (refactor: useNaverCallback 로직 변경 및 토큰 파싱 구현)
       });
   }, []);
 }
