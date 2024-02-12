aac363b33896ce88d38be3100aceff6924d7d0ad
이호진 <skwo777@naver.com>
2024-02-13 01:17:03 +0900

    feat: 이미지 로드 후 revoke 구현

--- front/src/hooks/useLoadImg.jsx
+++ front/src/hooks/useLoadImg.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { loadImg } from '../api/apis';
 
+/** loadImage에
+ * savePath, saveName을 가진 객체 또는 객체리스트를 넣으면
+ * 이미지 링크를 data로 반환하는 함수
+ */
 function useLoadImg() {
   const [data, setData] = useState([]);
   const loadImage = (list) => {
+    // 들어온 값을 무조건 리스트로 변환
     const itemList = Array.isArray(list) ? list : [list];
 
     itemList.forEach((item, index) => {
       const { savePath, saveName } = item;
       loadImg(savePath, saveName)
         .then((res) => {
-          console.log(res);
           const newPrev = [...data];
           const blob = new Blob([res.data], { type: 'image/jpeg' });
           const blobURL = URL.createObjectURL(blob);
@@ -20,6 +24,15 @@ function useLoadImg() {
         .catch(() => {});
     });
   };
+
+  useEffect(() => {
+    //언마운트시 이미지 해제
+    return () => {
+      data.forEach((item) => {
+        URL.revokeObjectURL(item);
+      });
+    };
+  });
   return [data, loadImage];
 }
 export default useLoadImg;
