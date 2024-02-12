5f117ded54494ee5fe380d91bc32fa898d2de2d2
엄진식 <ladiescheong@naver.com>
2024-02-13 00:29:21 +0900

    refactor: 데이터 불러오는 로직 개선

--- front/src/hooks/useLoadItem.jsx
+++ front/src/hooks/useLoadItem.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from 'react';
 import { getItem } from '../api/apis';
 
-function useLoadItem({ itemId }) {
+function useLoadItem(itemId) {
   const [goodsImageURL, setGoodsImageURL] = useState('');
   const [goodsName, setGoodsName] = useState('');
   const [goodsPrice, setGoodsPrice] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const { data } = await getItem(itemId); // itemId로 바꿔야 됨
-      console.log(data);
+      const { data } = await getItem(itemId);
       // setGoodsImageURL(); data.data.imageResponses
       setGoodsName(data.data.title);
       setGoodsPrice(data.data.price);
