a75317080d423d2ef7f516f8a9d3675bceaacf70
이호진 <skwo777@naver.com>
2024-02-14 14:21:20 +0900

    feat: 아이템 상태에 따른 버튼 상태 적용

--- front/src/components/molecules/GoodsHeader.jsx
+++ front/src/components/molecules/GoodsHeader.jsx
@@ -1,5 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import styled from 'styled-components';
 import font from '../../styles/font';
 import Button from '../atoms/Button';
@@ -8,13 +10,39 @@ import useLoadItem from '../../hooks/useLoadItem';
 import useLoadImg from '../../hooks/useLoadImg';
 
 function GoodsHeader({ itemId }) {
-  const [imageURL, name, price] = useLoadItem(itemId);
+  const [imageURL, name, price, itemStatus] = useLoadItem(itemId);
   const navigate = useNavigate();
   const { chatId } = useParams();
   const [img, loadImg] = useLoadImg();
+
+  const itemStatusArr = {
+    TRADE_RESERVE: {
+      status: 0,
+      content: '예약중',
+      warning: '예약된 물품이에요',
+    },
+    DP: { status: 1, content: 'DP 전시중', warning: '예약된 물품이에요' },
+    DP_RESERVE: {
+      status: 2,
+      content: 'DP 예정',
+      warning: '곧 DP 될 예정이에요!',
+    },
+    SOLD: { status: 3, content: '거래완료', warning: '거래된 물품이에요' },
+    WITHDRAW: {
+      status: 4,
+      content: '회수 대기중',
+      warning: '회수 예정인 물품이에요',
+    },
+    ONLINE: {
+      status: 5,
+      content: '거래 약속 하기',
+    },
+  };
+
   useEffect(() => {
     loadImg(imageURL);
   }, [imageURL]);
+
   return (
     <SFlex>
       <SImg src={img} alt="물건 사진" />
@@ -22,14 +50,22 @@ function GoodsHeader({ itemId }) {
         <SH2>{name}</SH2>
         <SPrice>{price}원</SPrice>
       </SDiv>
-      <Button
-        type="1"
-        onClick={() => {
-          navigate(`/station/${chatId}`);
-        }}
-      >
-        거래약속
-      </Button>
+      {itemStatus && (
+        <Button
+          type={itemStatusArr[itemStatus].status}
+          onClick={() => {
+            if (itemStatusArr[itemStatus].status === 5) {
+              navigate(`/station/${chatId}`);
+            } else {
+              toast.error(`${itemStatusArr[itemStatus].warning}`, {
+                position: 'top-center',
+              });
+            }
+          }}
+        >
+          {itemStatusArr[itemStatus].content}
+        </Button>
+      )}
     </SFlex>
   );
 }
