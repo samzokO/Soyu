0d4607b8a6eb5e769f266030cfa6d4233c1793a7
이호진 <skwo777@naver.com>
2024-02-14 17:22:23 +0900

    style: 내역 목록 motion 적용

--- front/src/components/molecules/BuyGoods.jsx
+++ front/src/components/molecules/BuyGoods.jsx
@@ -1,22 +1,65 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Badge from '../atoms/Badge';
 import Favorite from '../../assets/icons/material_24/favorite.svg';
 import Button from '../atoms/Button';
 import theme from '../../styles/theme';
 import { useTimeStamp } from '../../hooks/useTimeStamp';
 import useLoadImg from '../../hooks/useLoadImg';
+import useReservation from '../../hooks/useReservation';
 
-function BuyGoods({ data, list }) {
+function BuyGoods({ data, list, variants, itemStatus }) {
   const price = data.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   const [img, loadImage] = useLoadImg();
-  // const date = useTimeStamp(data.regDate);
+  const [r, reservation] = useReservation();
   useEffect(() => {
     loadImage(data.imageResponses[0]);
   });
+
+  const itemStatusList = {
+    TRADE_RESERVE: {
+      badge: 0,
+      status: list === 'buylist' ? 0 : 1,
+      content:
+        list === 'buylist'
+          ? '구매 코드 확인하기'
+          : '오프라인 판매 코드 확인하기',
+    },
+    DP: {
+      badge: 1,
+      status: list === 'buylist' ? 1 : 4,
+      content: list === 'buylist' ? '구매 코드 확인하기' : '회수 코드 확인하기',
+    },
+    DP_RESERVE: {
+      badge: 2,
+      status: list === 'buylist' ? 0 : 1,
+      content: list === 'buylist' ? 'DP 예정 물품입니다' : 'DP 코드 확인하기',
+    },
+    SOLD: {
+      badge: 3,
+      status: list === 'buylist' ? 4 : 3,
+      content: list === 'buylist' ? '구매목록에서 지우기' : '거래완료',
+    },
+    WITHDRAW: {
+      badge: 4,
+      status: list === 'buylist' ? 3 : 4,
+      content:
+        list === 'buylist' ? '회수 예정 물품입니다.' : '회수 코드 확인하기',
+    },
+    ONLINE: {
+      badge: 5,
+      status: list === 'buylist' ? 0 : 0,
+      content: list === 'buylist' ? '판매자와 채팅하기' : 'DP 예약하기',
+    },
+  };
+
+  const makeReservation = () => {
+    reservation(1, data.itemId);
+  };
   return (
-    <SLiWrap>
+    <SLiWrap variants={variants}>
       <Link to={`/item/${data.itemId}`}>
         <SSpaceBetween>
           <SFlexWrap>
@@ -30,11 +73,14 @@ function BuyGoods({ data, list }) {
               </SFlexWrap>
             </div>
           </SFlexWrap>
-          <Badge status={data.itemStatus === 'buyList' ? '3' : '5'} />
+          <Badge status={itemStatusList[itemStatus].badge} />
         </SSpaceBetween>
       </Link>
-      <Button Handler="" type={list === 'buylist' ? '1' : '0'}>
-        {list === 'buylist' ? '무튼구매' : '오프라인 DP 예약하기'}
+      <Button
+        onClick={makeReservation}
+        type={itemStatusList[itemStatus].status}
+      >
+        {itemStatusList[itemStatus].content}
       </Button>
     </SLiWrap>
   );
@@ -47,7 +93,7 @@ const SFlexWrap = styled.div`
   align-items: center;
 `;
 
-const SLiWrap = styled.li`
+const SLiWrap = styled(motion.li)`
   padding: 14px;
   position: relative;
   border-bottom: 1px solid ${theme.color.grayScale200};
@@ -61,6 +107,7 @@ const SSpaceBetween = styled(SFlexWrap)`
 
 const SImg = styled.img`
   width: 60px;
+  min-width: 60px;
   height: 60px;
   margin-right: 20px;
   /* background-image: url(${(props) => props.img}); */
@@ -69,6 +116,9 @@ const SImg = styled.img`
 const SH3 = styled.h3`
   ${theme.font.Subtitle};
   margin-bottom: 8px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const SPrice = styled.p`
