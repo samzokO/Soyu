f419469581c16ab073a52366c4babff4dae8a33a
이호진 <skwo777@naver.com>
2024-02-14 17:18:23 +0900

    style: motion 추가

--- front/src/components/molecules/InfoList.jsx
+++ front/src/components/molecules/InfoList.jsx
@@ -1,34 +1,55 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import font from '../../styles/font';
 import AccountWallet from '../../assets/icons/material_24/account_wallet.svg';
 import Edit from '../../assets/icons/material_24/edit.svg';
 import ImageSearch from '../../assets/icons/material_24/image_search.svg';
+import Modal from './modal/Modal';
+import {
+  transitionList,
+  transitionListitem,
+} from '../../styles/ListTransition';
 
 function InfoList() {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClickButton = () => {
+    setIsOpen(true);
+  };
   return (
     <>
       <SH2>내 정보</SH2>
-      <ul>
-        <SLi>
+      <motion.ul variants={transitionList} initial="hidden" animate="visible">
+        <SLi variants={transitionListitem}>
           <SFlexLink to="account">
             <SImg src={AccountWallet} alt="#" />
             <p>계좌번호 변경</p>
           </SFlexLink>
         </SLi>
-        <SLi>
-          <SFlexLink to="/">
+        <SLi variants={transitionListitem}>
+          <SFlexLink onClick={onClickButton}>
             <SImg src={ImageSearch} alt="#" />
             <p>프로필 이미지 변경</p>
+            {isOpen && (
+              <Modal
+                open={isOpen}
+                onClose={() => {
+                  console.log(isOpen);
+                  setIsOpen(false);
+                  console.log(isOpen);
+                }}
+              />
+            )}
           </SFlexLink>
         </SLi>
-        <SLi>
+        <SLi variants={transitionListitem}>
           <SFlexLink to="/auth">
             <SImg src={Edit} alt="#" />
             <p>닉네임 변경</p>
           </SFlexLink>
         </SLi>
-      </ul>
+      </motion.ul>
     </>
   );
 }
@@ -40,7 +61,7 @@ const SH2 = styled.h2`
   margin: 20px 0 16px;
 `;
 
-const SLi = styled.li`
+const SLi = styled(motion.li)`
   margin-bottom: 11px;
 `;
 
