21fae0724117700b64accf0dc9c9d257eb9862b6
이호진 <skwo777@naver.com>
2024-02-14 12:28:07 +0900

    refactor: button Onclick 적용

--- front/src/components/atoms/TextBtn.jsx
+++ front/src/components/atoms/TextBtn.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
-function TextBtn({ children }) {
-  return <SBtn>{children}</SBtn>;
+function TextBtn({ children, type, onClick }) {
+  return (
+    <SBtn type={type} onClick={onClick}>
+      {children}
+    </SBtn>
+  );
 }
 
 const SBtn = styled.button``;
